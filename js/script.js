6c1b7256a90b38b647b4a68cc7c7d52026aec73d
radhikaprabu <radhikaprabu91699@gmail.com>
2016-04-06 13:04:06 -0500

    Weather App

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
+$(function () {
+
+
+	//Function to get json data
+	function getWeatherData(zipcode) {
+		var apikey = $('#apikey').val();
+		/*if (apikey != "") {
+			saveKey();
+		};*/
+		console.log("apikey=" + apikey);
+		console.log("zipcode=" + zipcode);
+		var weatherApi = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=' + apikey;
+		$.getJSON(weatherApi)
+			.done(function (data) {
+			addNewItem(data);
+			}).fail(function () {
+				$('#form').append('<p> Can not load data </p>');
+			});
+	}
+	
+	//Event handler to get the data
+	$('form').on('submit', function (e) {
+		e.preventDefault();
+		$('#info').remove();
+		getWeatherData($('#zip').val());
+	});
+	
+	//Function to add weather data to the page
+	function addNewItem(data){
+		var item = '';
+		item += '<h3>The weather info for' + ' ' + data.name + '</h3>';
+		item += '<p>Current Temperature: ' + convertTemp(data.main.temp) + '</p>';
+		item += '<p>' + data.weather[0].description + '</p>';
+		$('#form').append('<div id = info>' + item + '</div>');
+	}
+	
+	//Function to convert Temperature
+	function convertTemp (temp){
+		return Math.round((temp * 1.8) - 459.67);
+	}
+	
 });
+	
+	
+
+	
